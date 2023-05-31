3cbceed50b6d3eea4653b9c7e84cf68aa8e623bc
EmilKulka <226310@student.uek.krakow.pl>
2023-05-31 21:04:49 +0200

    templates EK

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,24 @@
 from app import app
+from flask import Flask, render_template
+
 
 @app.route("/name", defaults={"name":"Anonim"})
 @app.route("/name/<name>")
 def name(name):
     return f"Hello {name}"
+
+@app.route("/")
+def main_page():
+    return render_template("index.html")
+
+@app.route("/Ekstrakcja")
+def extraction():
+    return render_template("extraction.html")
+
+@app.route("/Autor")
+def author():
+    return render_template("author.html")
+
+@app.route("/Lista_produktów")
+def product_list():
+    return render_template("product_list.html")
